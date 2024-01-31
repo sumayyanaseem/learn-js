c0b2a05c99f77e844c09e8715100482734ee3bff
sumayyanaseem <naseem.s@northeastern.edu>
2024-01-31 10:36:45 -0800

    fixed errors

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,15 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    if (this && this.errMsg) {
+        console.error(this.errMsg);
+    } else {
+        console.error("Error message not found in the provided object");
+    }
+}
+
+// Example usage:
+const errorObject = { errMsg: "This is an error message." };
+logMsg.call(errorObject);
+
+const anotherObject = { message: "This object does not have errMsg." };
+logMsg.call(anotherObject);
