656230ab706fa8e9362a2c0d34f2509cb2da8121
sumayyanaseem <naseem.s@northeastern.edu>
2024-02-05 19:40:15 -0800

    changes related to dom manipulation, exceptions and map

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -12,6 +12,7 @@ function createTRNode(colNodes) {
 
 function createTDNode(childNode) {
   let tdNode = document.createElement("td");
+  tdNode.classList.add('tdNode');
   tdNode.appendChild(childNode);
   return tdNode;
 }
@@ -20,11 +21,28 @@ function createTxtNode(txt) {
   let txtNode = document.createTextNode(txt);
   return txtNode;
 }
+function createBtnNode(txt,e,l){
+let btnNode = document.createElement("button");
+let btnNodeText = createTxtNode(txt);
+btnNode.appendChild(btnNodeText);
+btnNode.addEventListener(e,l);
+return btnNode;
+}
 
+function edit(tdNode){
+let newNode = document.createElement("input");
+newNode.type='text';
+newNode.placeholder='Enter cell (x,y)....';
+tdNode.replaceChild(newNode,tdNode.childNodes[0]);
+}
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
+    let col2 = createTDNode(createBtnNode("Edit Text","click",function(){
+        edit(col1);
+        }));
+
     tableNode.appendChild(createTRNode([col1]));
   }
   document.getElementById("root").appendChild(tableNode);
